84b4d131fa5352d8cf35ef7abd51355fe81c8420
Jasper Spahl <jasperspahl@web.de>
2021-10-16 01:04:25 +0200

    Fixed the seeds

--- seeds/state.js
+++ seeds/state.js
@@ -4,12 +4,15 @@ const PresenceStates = require('../src/constants/PresenceStatus');
  * @param {Knex} knex
  */
 exports.seed = async (knex) => {
-  // Deletes ALL existing entries
-  await knex(tableNames.state).del();
-  // Inserts seed entries
-  const stateData = [];
-  for (const state in PresenceStates) {
-    stateData.push(PresenceStates[state]);
+  const data = await knex(tableNames.state).select('id');
+  if (data[0].id != 1) {
+    console.log('deleting data');
+    await knex(tableNames.state).del();
+    console.log('creating data');
+    const stateData = [];
+    for (const state in PresenceStates) {
+      stateData.push(PresenceStates[state]);
+    }
+    await knex(tableNames.state).insert(stateData);
   }
-  await knex(tableNames.state).insert(stateData);
 };
